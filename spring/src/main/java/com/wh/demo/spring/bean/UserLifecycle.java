41df025f54c10d135db80918b71c12b3a1adf61c
wanghao4 <Wh996399>
2021-01-30 05:30:28 +0800

    上传一些spring的相关知识点的验证实例

--- spring/src/main/java/com/wh/demo/spring/bean/UserLifecycle.java
+++ spring/src/main/java/com/wh/demo/spring/bean/UserLifecycle.java
@@ -3,6 +3,12 @@ package com.wh.demo.spring.bean;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -10,15 +16,35 @@ import javax.annotation.PreDestroy;
 @Data
 @NoArgsConstructor
 @SuperBuilder
-public class UserLifecycle extends User {
+public class UserLifecycle extends User implements BeanNameAware, BeanFactoryAware, InitializingBean, DisposableBean {
 
     @PostConstruct
-    public void init() {
+    public void postConstruct() {
         System.out.println("UserLifecycle->PostConstruct");
     }
 
     @PreDestroy
-    public void destroy() {
+    public void preDestroy() {
         System.out.println("UserLifecycle->PreDestroy");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("UserLifecycle->beanFactory");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("UserLifecycle->name:" + name);
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("UserLifecycle->destroy");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("UserLifecycle->afterPropertiesSet");
     }
 }
